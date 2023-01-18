6b96c9f19b48a6b4b12441442939d71b7c7b2a7d
Can Kologlu <mckologlu@gmail.com>
2023-01-18 15:16:55 -0500

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
+const wordSearch = (letters, word) => {
+    const transposedLetter = transpose(letters);
+  
+    return (checkWord(letters, word) || checkWord(transposedLetter, word)) || false;
+  };
+  
+  const checkWord = (data,word) => {
+    const wordJoin = data.map(ls => ls.join(''));
+    for (let l of wordJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  const transpose = function(matrix) {
+    let results = [];
+    for (let column = 0; column < matrix[0].length; column++) {
+      let rows = [];
+      for (let row = 0; row < matrix.length; row++) {
+        rows.push(matrix[row][column]);
+      }
+      results.push(rows);
+    }
+    return results;
+  };
 module.exports = wordSearch
